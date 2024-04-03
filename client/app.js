ad5de1b44766e9a664ca2bc2c37326829dd4d6a8
ucemrecan <ucemrecan@gmail.com>
2024-04-03 21:37:15 +0300

    Added cors dependency

--- client/app.js
+++ client/app.js
@@ -2,6 +2,8 @@ const url = document.getElementById("url");
 const previewButton = document.getElementById("preview-button");
 const errorMessage = document.getElementById("error-message");
 
+const baseURL = "http://localhost:5000";
+
 function extractVideoId(url) {
   if (url.includes("v=")) {
     return url.split("v=")[1];
@@ -9,10 +11,17 @@ function extractVideoId(url) {
 }
 
 previewButton.addEventListener("click", async () => {
+  let isLoaded = false;
   const videoId = extractVideoId(url.value);
 
   if (!videoId) {
     errorMessage.textContent = "Required url parameter is missing *";
     return;
   }
+
+  const response = await fetch(
+    `${baseURL}/youtube/metadata?videoId=${videoId}`
+  );
+  const data = await response.json();
+  isLoaded = true;
 });
