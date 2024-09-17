a51a05fdf3b535a7402103503d5bd729dc5b8335
ucemrecan <ucemrecan@gmail.com>
2024-09-17 17:00:18 +0300

    Swagger docs created

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 import express from "express";
 import routes from "./routes/index.js";
 import cors from "cors";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 
 const app = express();
 const PORT = 5000;
 
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Youtube Download API",
+      version: "1.0.0",
+    },
+    servers: ["http://localhost:5000"],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,8 +9,48 @@ router.get("/healthz", (req, res) => {
   res.status(200).send("ok");
 });
 
+/**
+ * @swagger
+ * /youtube/metadata:
+ *   get:
+ *     description: Get metadata of a youtube video
+ *     parameters:
+ *       - name: videoId
+ *         description: The youtube video id
+ *         in: query
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: A successful response
+ *       400:
+ *         description: Bad request
+ */
 router.get("/youtube/metadata", videoController.getYoutubeMetadata);
 
+/**
+ * @swagger
+ * /youtube/download:
+ *   post:
+ *     description: Download a youtube video
+ *     parameters:
+ *      - in: body
+ *        name: videoId
+ *        description: The youtube video id
+ *        schema:
+ *          type: object
+ *          properties:
+ *            videoId:
+ *             type: string
+ *             required: true
+ *             example: "wCoI2SGjaq4"
+ *
+ *     responses:
+ *       200:
+ *         description: A successful response
+ *       400:
+ *         description: Bad request
+ */
 router.post("/youtube/download", videoController.downloadYoutubeVideo);
 
 export default router;
